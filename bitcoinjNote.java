080ce937ae2cdd3ac937cddeb0e0caad2328932e
liupan163 <18792418363@163.com>
2019-01-16 15:56:45 +0800

    add |> add file

--- bitcoinjNote.java
+++ bitcoinjNote.java
@@ -23,7 +23,7 @@
 propTx.keyPaths.put(输出公钥脚本，公钥路径)
 
 
-之前：       inputScript = txIn.getScriptSig(); //incomplete inputScript with OP_0 in place of one or more signatures
+//之前：       inputScript = txIn.getScriptSig(); //incomplete inputScript with OP_0 in place of one or more signatures
 
 byte[] script = redeemData.redeemScript.getProgram();
 
@@ -34,13 +34,16 @@ signature = tx.calculateSignature(i, key, script, Transaction.SigHash.ALL, false
 // hashType === a part of a scriptSig signature on the inputs
 // anyoneCanPay
 
-签名脚本:    inputScript = scriptPubKey.getScriptSigWithSignature(inputScript, signature.encodeToBitcoin(), sigIndex);
+//签名脚本:    inputScript = scriptPubKey.getScriptSigWithSignature(inputScript, signature.encodeToBitcoin(), sigIndex);
 //getScriptSigWithSignature
-加入签名脚本：txIn.setScriptSig(inputScript);
+//加入签名脚本：txIn.setScriptSig(inputScript);
 
-//OP_RETURN is a script opcode used to mark a transaction output as invalid.
-//OP_RETURN outputs can be used to burn bitcoins
-//脚本操作符，无效交易标记--目的是为了在比特币中存储信息，方法是在交易输出脚本中加入OP_RETURN标志
+/*
+OP_RETURN is a script opcode used to mark a transaction output as invalid.
+OP_RETURN outputs can be used to burn bitcoins
+脚本操作符，无效交易标记--目的是为了在比特币中存储信息.
+方法是在交易输出脚本中加入OP_RETURN标志&&OP_RETURN一笔交易里面只能有一个
+*/
 
 
 
@@ -53,11 +56,19 @@ signature = tx.calculateSignature(i, key, script, Transaction.SigHash.ALL, false
 *   设置fast catchup起始时间    <li>Set the fast catchup time using {@link PeerGroup#setFastCatchupTimeSecs(long)}, to optimize chain download.</li>
 *</ol>
 */
+/*
 接受tx交易广播，对应
         peerGroup中, wallet.setTransactionBroadcaster(this)
         ......
         tx.broadcast = broadcaster.broadcastTransaction(tx)
+*/
 
 
+//isMine（）方法，作用:返回这个输出是个key，还是个钱包里面的地址
 
-isMine（）方法，作用:返回这个输出是个key，还是个钱包里面的地址
+//txFee计算：  //false --- 发送者付，  true --- 接受者付，多个接受者时候，均摊splite
+//public boolean recipientsPayFees = false;
+/**
+ * If false (default value), tx fee is paid by the sender If true, tx fee is paid by the recipient/s. If there is
+ * more than one recipient, the tx fee is split equally between them regardless of output value and size.
+ */
